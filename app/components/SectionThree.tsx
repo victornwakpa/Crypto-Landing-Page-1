0760d73b1824d8656213e0c3f50fbe5f9e825f3f
victornwakpa <nwakpavictor@gmail.com>
2023-11-16 04:31:18 +0100

    minor changes

---
+++ app/components/SectionThree.tsx
@@ -0,0 +1,7 @@
+export default function SectionThree() {
+    return (
+        <div>
+            <div className="py-20"></div>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from './components/Hero'
+import SectionThree from './components/SectionThree'
 import SectionTwo from './components/SectionTwo'
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
    <div className="bg-slate-100 text-gray-800">
     <Hero />
     <SectionTwo />
+    <SectionThree />
    </div>
   )
 }
