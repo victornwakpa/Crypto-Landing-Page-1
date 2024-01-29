14e5f6b4fd2508352f6b6243033381af0ec90239
victornwakpa <nwakpavictor@gmail.com>
2024-01-29 06:23:32 +0100

    fixed responsiveness

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     return (
         <div className="flex flex-col gap-8 md:gap-0 md:flex-row bg-white py-20 px-5 md:px-20">
             <div>
-                <div className="flex flex-col gap-5 md:px-12">
+                <div className="flex flex-col gap-5 md:pr-12">
                     <div className="text-center">
                     <Image src={logo} alt="image showing sign up" className="inline-block w-64" />
                     <p className="mt-2 text-center md:text-left">Take your crypto to the next level</p>
@@ -44,6 +44,17 @@ export default function Footer() {
                         <p>University</p>
                         <p>Research</p>
                         <p>Market Updates</p>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-3">
+                    <h3 className="font-semibold">Products</h3>
+                    <div className="text-gray-600 flex flex-col gap-3">
+                        <p>Stock & Fund</p>
+                        <p>Cash Card</p>
+                        <p>Crypto</p>
+                        <p>Options</p>
+                        <p>Gold</p>
+                        <p>Learn Snacks</p>
                     </div>
                 </div>
                 <div className="flex flex-col gap-3">
